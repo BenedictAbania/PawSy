64e6946de995e283820cd56b3fe6d8f02ba8c2e5
callMeJZ <coronadojessa.lshs@gmail.com>
2025-10-25 01:12:36 +0800

    Add Account and modified pages, json, and folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
+import LandingPage from "./pages/LandingPage";
+import Shop from "./pages/Shop";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import Account from "./pages/Account";
+import EditProfile from "./pages/EditProfile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<LandingPage />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
