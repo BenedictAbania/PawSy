03cc1045e78bd4afa4a6e169bd51c4754a4807a8
BenedictAbania <abaniachristian@gmail.com>
2025-10-25 23:06:34 +0800

    Task 2 Merge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -45,7 +22,6 @@ function App() {
       </Routes>
       <Footer />
     </BrowserRouter>
->>>>>>> origin/Task2
   );
 }
 
